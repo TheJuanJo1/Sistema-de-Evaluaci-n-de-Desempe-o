313906277a80e1fd63783d65382a92568db7f62b
The_JuanJo1 <mbappejuan464@gmail.com>
2026-06-17 13:26:06 -0500

    Se hizo el archivo para poder cambiar como de idioma el restablecimiento de contaseña

---
+++ evaluacion-system/resources/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Tu contraseña ha sido restablecida!',
+    'sent' => '¡Se ha enviado el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor, espere antes de intentar nuevamente.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo.",
+];
